643ca1dedd58c79dd6a41c86ce6456050b7f7eba
PushpaVJ <pushpa_vj@rediffmail.com>
2023-03-12 00:37:38 +0530

    execution completed

--- CNN_Transform_learning_VGG16.py
+++ CNN_Transform_learning_VGG16.py
@@ -13,7 +13,16 @@ import urllib.request as request
 #This module is used to download the and and bees image data set from the
 #web site. This data set is used to train the neural network using the VGG16
 # CNN architecture transfer learning algorithm.
-
+#In this module first we download the VGG16 model which is pretrained model for 1000 classifications.
+#Then we modify this VGG16 model to be able to classify the bees images as 2 number of classifications.
+#Then we will train the neural network using the modified VGG16 model.
+#Then we will save the trained neural network to a file.
+#The size of the model saved will be of huge i.e. 500 mb
+# So in order to reduce the size of the model we need to implement TFLite which donwloads the above
+#saved model and converts it to a.tflite file. The size of the .tflite will be of reduced one around 50MB
+#Further we can reduce the size of the .tflite model by using the model optimizer by convertint the
+#tflite model to a binary object model via quantized model.
+#For this we have written TFlite.py module
 #****************************************************************************
 
 IMAGE_SIZE = (224, 224)
@@ -24,7 +33,9 @@ model = tf.keras.applications.VGG16(weights='imagenet',
                     input_shape=None, 
                     classes=1000 #pre trained model has 1000 classes
 
-                     ) 
+                     ) #This will download the VGG16 model which is pretrained model 
+                        #for 1000 classifications
+
 
 print("model summary", model.summary())
 
@@ -39,19 +50,22 @@ def create_dir(dir_path):
     os.makedirs(dir_path,exist_ok=True)
     print(f"Directory {dir_path} created")
         
-ROOT_DATA_DIR="data"
+ROOT_DATA_DIR="data" #directory where the data will be downloaded
+
 create_dir(ROOT_DATA_DIR)
 
-data_zip_file="data.zip"
+data_zip_file="data.zip" #name of the zip file where the data will be downloaded
+
 data_zip_path=os.path.join(ROOT_DATA_DIR,data_zip_file)
 
-if not os.path.isfile(data_zip_path):
+if not os.path.isfile(data_zip_path): #check if the data zip file is already downloaded
     print("downloading data")
     filename,headers = request.urlretrieve(data_url, data_zip_path)
     print(f"filename: {filename} created with info: {headers}")
 else:
     print ("data already downloaded")
-    print()
+  
+
 
 #unzip the data
 from zipfile import ZipFile
@@ -59,7 +73,8 @@ from zipfile import ZipFile
 def unzip_data(source:str,dest:str):
     with ZipFile(source, 'r') as zip_ref:
         zip_ref.extractall(dest)
-unzip_data(data_zip_path,ROOT_DATA_DIR)
+unzip_data(data_zip_path,ROOT_DATA_DIR) #unzip the data to the Root directory
+
 
 main_data_dir=Path(r"D:\user\jupyternotes\Praketh\CV-Computer vision\CNN_VGG16_translearning\data\hymenoptera_data\train")
 #Path is a library that allows you to create and manipulate file paths.
@@ -74,12 +89,15 @@ IMAGE_SIZE=(PIXCEL_PER_IMAGE,PIXCEL_PER_IMAGE)
 #create data generators like data loaders in pytorch
 dict()
 datagen_keyword = dict(rescale=1./255,validation_split=0.20) # data generator
-     #key workd arguments for data generator
-#the parameter rescale is to multiply every pixel in the preprocessing 
+     #key word arguments for data generator
+#the parameter rescale is to multiply every pixel in the preprocessing to resize the image
 # image. rescale: rescaling factor.
 dataflow_kwargs=dict(target_size=IMAGE_SIZE,
                      batch_size=BATCH_SIZE,
-                     )#data flow keyword arguments
+                     )#data flow keyword arguments to the data generator with the target_size 
+                      #with the given batch_size
+
+
 valid_datagen=tf.keras.preprocessing.image.ImageDataGenerator(
                             **datagen_keyword) #This will create a valid data generator
 
@@ -90,9 +108,11 @@ valid_generator=valid_datagen.flow_from_directory(
                                 subset='validation',
                                 shuffle=False,
                                 **dataflow_kwargs)
-#The flow_from_directory() method takes a path of a directory 
+#The flow_from_directory() method takes a path of a train data directory 
 # and generates batches of augmented data. 
 data_agumentation=False 
+
+#perform data augmentation if the data is a training data set
 
 if data_agumentation:
     train_datagen=tf.keras.preprocessing.image.ImageDataGenerator(
@@ -108,9 +128,11 @@ else:
     
 train_generator=train_datagen.flow_from_directory(
                 main_data_dir,subset="training",shuffle = True,
-                **dataflow_kwargs)
+                **dataflow_kwargs) #this will generate batches of augmented data
+
 RGB_IMAGE_SIZE=(PIXCEL_PER_IMAGE,PIXCEL_PER_IMAGE,3)
 
+#download the VGG16 model till the convolution layer for the required image_shape or size
 vgg= tf.keras.applications.vgg16.VGG16(
     input_shape=RGB_IMAGE_SIZE,
     weights='imagenet',
@@ -122,38 +144,53 @@ vgg= tf.keras.applications.vgg16.VGG16(
 
 print("vgg summary", vgg.summary())
 
-for layer in vgg.layers:
-    print(f"layer name: {layer.name} is trainable: {layer.trainable}")
-    layer.trainable=False
 #freeze all the layers
+for layer in vgg.layers:
+    print(f"layer name: {layer.name} is trainable: {layer.trainable}")
+    layer.trainable=False # freeze all the layers by setting trainable as False
 
 
+#check if the layers are frozen or not
 
 for layer in vgg.layers:
     print(f"layer name: {layer.name} is trainable: {layer.trainable}")
+
+print("vgg.output",vgg.output) # this will print the fully connected layer
+
+
+CLASSES=2 #set the number of classes for the model
+
+x=tf.keras.layers.Flatten()(vgg.output)# This will flatten the fully connected layer
+
+predictions=tf.keras.layers.Dense(CLASSES,activation='softmax')(x) #This will create the output layer
 
 print("vgg.output",vgg.output)
+print("predictions",predictions)
 
-CLASSES=2
-x=tf.keras.layers.Flatten()(vgg.output)
-predictions=tf.keras.layers.Dense(CLASSES,activation='softmax')(x)
+new_model=tf.keras.models.Model(inputs=vgg.input,outputs=predictions) # this will create a new model
+                        #based on the required input image size and number of classes
 
-new_model=tf.keras.models.Model(inputs=vgg.input,outputs=predictions)
 print("new_model summary",new_model.summary())
 
 new_model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy']) 
 
-EPOCHS=10
+EPOCHS=10 
+#for the number of epoches the training data and validation data will be generated for the 
+# given number of batches and the model will be trained for the given number of epochs
+
 history=new_model.fit(train_generator,
                                 epochs=EPOCHS,
                                 validation_data=valid_generator,
 )         
                 
-new_model.save('CNN_VGG16_new_full_model.h5')
+new_model.save('CNN_VGG16_new_full_model.h5') #save the new model
 
-print("train_generator.class_indices",train_generator.class_indices)
+
+print("train_generator.class_indices",train_generator.class_indices) #interchange class name and 
+                                                                    # values
+
 
 label_map={val: key for key, val in train_generator.class_indices.items()}
 
@@ -175,17 +212,23 @@ print("test_image.shape",test_image.shape)
 resized_img=tf.image.resize(test_image,(224,224))
 
 #need to expand the image to be able to use it in the model
-input_image=tf.expand_dims(resized_img,axis=0)
+input_image=tf.expand_dims(resized_img,axis=0) #expand will add 1 diminsion at the beginning to the
+                                               #image shape
 
-pred=new_model(input_image)
+pred=new_model(input_image) #predict the given image.i.e. provides the probability of each output
+                            # class here it is 2 classes ants and bees
 print("pred.shape",pred.shape)
-print("pred",pred)
+print("pred",pred) 
 
-pred_index=np.argmax(pred,axis=1)
+pred_index=np.argmax(pred,axis=1) #select the index of the highest probability
+
 print("predicted value",label_map[pred_index[0]])
 
+#download the saved new model
 loaded_model=tf.keras.models.load_model('CNN_VGG16_new_full_model.h5')
-preprocessed_image=tf.keras.applications.vgg16.preprocess_input(test_image)
+preprocessed_image=tf.keras.applications.vgg16.preprocess_input(test_image) #preprocess_input is a
+#function that takes an image and returns a preprocessed image
+
 
 print("preprocessed_image.shape",preprocessed_image.shape)
 
--- TFlite.py
+++ TFlite.py
@@ -4,35 +4,55 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from pathlib import Path
 import os
-
+#***************************************************************************************************
+# The function of this module is to TFlite model inference. And also optimized model inference.
+# In this module, we will download the VGG16 model which was created in the
+# CNN_Transform_learning_VGG16.py module.
+# Then we will convert this VGG model to TFlite model.
+# The benifit of converting to TFlite model is that the size of the model
+# will reduce dastrically from 500MB to 50MB around.
+# To further reduce the size of the model, we will build the optimized model
+# via building the quantized model.
+# The quantized model will conver the model into binary object.
+# In this model we will also see how to use TFlite and quantized model
+# to predict the class of an image.
+#***************************************************************************************************
+#Get the path of the VGG16 new model
 path_of_vgg16_trained_model=Path(r"D:\user\jupyternotes\Praketh\CV-Computer vision\CNN_VGG16_translearning\CNN_VGG16_new_full_model.h5")
+# Download the VGG16 new model
+
 model=tf.keras.models.load_model(path_of_vgg16_trained_model)
 
-#create the coverted model
+#create the converted TFliet model
 converter=tf.lite.TFLiteConverter.from_keras_model(model)
 #this will create the converted model
 
 tflite_model=converter.convert()
 #this will create the tflite model
 
-#save this tflite model
+#save this tflite model under new directory tflite_model_dir
 
 tflite_model_path=Path("./tflite_model_dir")
-tflite_model_path.mkdir(parents=True, exist_ok=True) #create the directory if it doesn't exist
+tflite_model_path.mkdir(parents=True, exist_ok=True) 
+#create the directory if it doesn't exist
 
-tfliet_model_file=tflite_model_path/"vgg16_model.tflite"
+#save the tflite model
+
+tfliet_model_file=tflite_model_path/"vgg16_model.tflite" 
 tfliet_model_file.write_bytes(tflite_model)
 #The tflite model is saved in the tflite_model_dir folder
 #The tfliet model is created to reduce the size of the model
 
-#To further reduce the size of the model, we need to convert the tflite model to a binary model
+#To further reduce the size of the model, we need to convert the 
+# tflite model to a binary model
 converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
 tflite_quant_model=converter.convert()
 
 #now we need to quantize the tflite model
 
 tflite_model_quant_file=tflite_model_path/"vgg16_quant_model.tflite"
-tflite_model_quant_file.write_bytes(tflite_quant_model) #quantized model is saved in the tflite_model_dir folder
+tflite_model_quant_file.write_bytes(tflite_quant_model) 
+#quantized model is saved in the tflite_model_dir folder
 
 
 #Verifying the quantized model
@@ -61,7 +81,8 @@ print(interpreter.get_input_details()[0])
 print(interpreter.get_input_details()[0]["shape"])
 
 #To check what is the shape of the input tensor
-
+#The shape of the input tensor is [  1 224 224   3]
+#We need to change the shape of the test image to [1,224,224,3]
 #print(tf.image.resize(test_image,(224,224)).shape)
 resized_image=tf.image.resize(test_image,(224,224))
 
@@ -71,28 +92,34 @@ print("inputa_data.shape",input_data.shape)
 input_index=interpreter.get_input_details()[0]["index"]
 output_index=interpreter.get_output_details()[0]["index"]
 print("output_details",interpreter.get_output_details())
-
+#Out put details gives the details of the output shape that indicates the 
+# number of prediction classes it will be producing.
 print("output_index",output_index)
-
+#pass the test image to the interpreter at its input index
 interpreter.set_tensor(input_index,input_data)
 interpreter.invoke()
+#predict the class of the input image
 pred= interpreter.get_tensor(output_index)
+#gives the probability of the class of the input image
 print("pred is",pred)
 
 label_map={0:"ants",1:"bees"}
-
+#get the predicted class by taking the argmax of the probability
 argmax=tf.argmax(pred[0]).numpy()
 print("argmax is",argmax)
 print(label_map[argmax])
-
+#predict the class of the input image using quantized model
 input_index_quant=interpreter_quant.get_input_details()[0]["index"]
 output_index_quant=interpreter_quant.get_output_details()[0]["index"]
-
+#pass the test image to the interpreter at its input index
 interpreter.set_tensor(input_index_quant,input_data)
 interpreter.invoke()
 
 pred_quant= interpreter.get_tensor(output_index_quant)
+#gives the probability of the class of the input image
+print("pred is",pred)
 print("pred is",pred_quant)
+#get the predicted class by taking the argmax of the probability
 argmax_quant=tf.argmax(pred_quant[0]).numpy()
 print(label_map[argmax_quant])
 
